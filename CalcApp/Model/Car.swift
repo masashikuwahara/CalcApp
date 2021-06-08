19d7a663173213d3ee717c4eeb19547e590e000d
桑原雅史 <brucemclarenp4@gmail.com>
2021-06-08 18:10:31 +0900

    first commit

---
+++ CalcApp/Model/Car.swift
@@ -0,0 +1,8 @@
+//
+//  Car.swift
+//  CalcApp
+//
+//  Created by 桑原雅史 on 2021/06/08.
+//
+
+import Foundation
---
+++ CalcApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CalcApp
+//
+//  Created by 桑原雅史 on 2021/06/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        
+    }
+
+
+}
+
