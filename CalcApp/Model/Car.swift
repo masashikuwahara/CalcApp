a3a30c0837f27fd90334c3b11370c79f38aa4e94
桑原雅史 <brucemclarenp4@gmail.com>
2021-06-08 19:50:54 +0900

    クラス、メソッドを定義

--- CalcApp/Model/Car.swift
+++ CalcApp/Model/Car.swift
@@ -16,15 +16,33 @@ class Car{
     //イニシャライズ = 初期化
     init(){
         
-        frontwheel = 2
-        rearwheel = 2
+        frontwheel = 0
+        rearwheel = 0
         
     }
     
+    //機能
     func drive(){
         
+        print("運転開始！！")
+        print("前輪  \(frontwheel)")
+        print("後輪  \(rearwheel)")
+        
         
     }
     
+    
+    func move(toBack:String){
+        
+        print(toBack)
+        
+    }
+    
+    func plusAndMinus(num1:Int,num2:Int)->Int{
+        
+        return num1 + num2
+        
+        
+    }
     
 }
